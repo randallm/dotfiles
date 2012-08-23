03c1e4ef735e70dd04682a46dc6572db5b203b09
Randall Ma <randall@randallma.com>
2012-08-23 16:26:49 -0700

    add syncto script

---
+++ syncto.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "Re-adding bash..."
+cp ~/.bashrc .bashrc
+
+echo "Re-adding conky..."
+cp ~/.conkyrc .conkyrc
+
+echo "Re-adding redshiftgui..."
+cp ~/.redshiftgrc .redshiftgrc
+
+echo "Re-adding vim..."
+cp ~/.vimrc .vimrc
+
+echo "Re-adding X init..."
+cp ~/.xinitrc .xinitrc
+
+echo "Re-adding X display..."
+cp ~/.Xresources .Xresources
+
+echo "Re-adding Sublime Text 2..."
+cp -r ~/.config/sublime-text-2/ sublime-text-2/
+
+echo ""
+echo "All done!"
+
